261855d9bf8adecced4aeb8b5d4008785f0fa93c
Ing. Alejandro Soto <soto@itsaltillo.edu.mx>
2020-02-26 19:53:27 -0600

    Validación de Formularios y Snackbar

--- design_app2/lib/login_page.dart
+++ design_app2/lib/login_page.dart
@@ -2,12 +2,22 @@ import 'package:design_app2/place_page.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class LoginPage extends StatelessWidget{
+class LoginPage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState()=> new _LoginPageState();
+
+}
+class _LoginPageState extends State<LoginPage>{
+  String _user = "";
+  String _password = "";
+  var _formKey = GlobalKey<FormState>();
+  var _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
+      key: _scaffoldKey,
       body: Container(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
@@ -29,12 +39,7 @@ class LoginPage extends StatelessWidget{
                     fontWeight: FontWeight.w700,
                       fontSize: 58
                   )
-                )/*TextStyle(
-                  color: Colors.blue,
-                  fontWeight: FontWeight.w700,
-                  fontFamily: "Signatra",
-                  fontSize: 58
-                )*/
+                )
               )
             ),
             Padding(
@@ -49,112 +54,121 @@ class LoginPage extends StatelessWidget{
                 ),
               ),
             ),
-            Padding(
-              padding: EdgeInsets.only(left: 8, top:16),
+            Container(
               child: Form(
+                key: _formKey,
                 child: Column(
                   children: <Widget>[
-                    Container(
-                      width: 300,
-                      child: Material(
-                        elevation: 5.0,
-                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                        color: Colors.blue,
-                        child: Row(
+                    Padding(
+                        padding: EdgeInsets.only(left: 8, top:16),
+                        child: Column(
                           children: <Widget>[
-                            Padding(
-                              padding: EdgeInsets.all(12),
-                              child: Icon(Icons.account_circle,
-                                color: Colors.white,
-                              ),
-                            ),
                             Container(
-                              width: 252,
-                              height: 60,
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                  borderRadius: BorderRadius.only(
-                                      bottomRight: Radius.circular(10),
-                                    topRight: Radius.circular(10)
-                                  )
-                              ),
-                              child: Padding(
-                                padding: EdgeInsets.all(8),
-                                child: TextFormField(
-                                  style:TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 18
-                                  ),
-                                  decoration: InputDecoration(
-                                    border: InputBorder.none,
-                                    hintText: "Usuario",
-                                    fillColor: Colors.white,
-                                    filled: true
-                                  ),
+                              width: 300,
+                              child: Material(
+                                elevation: 5.0,
+                                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                                color: Colors.blue,
+                                child: Row(
+                                  children: <Widget>[
+                                    Padding(
+                                      padding: EdgeInsets.all(12),
+                                      child: Icon(Icons.account_circle,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                    Container(
+                                      width: 252,
+                                      height: 60,
+                                      decoration: BoxDecoration(
+                                          color: Colors.white,
+                                          borderRadius: BorderRadius.only(
+                                              bottomRight: Radius.circular(10),
+                                              topRight: Radius.circular(10)
+                                          )
+                                      ),
+                                      child: Padding(
+                                        padding: EdgeInsets.all(8),
+                                        child: TextFormField(
+                                          validator: (value)=> value.isEmpty ? "Usuario es obligatorio!": null,
+                                          onSaved: (value) => _user = value,
+                                          style:TextStyle(
+                                              color: Colors.black,
+                                              fontSize: 18
+                                          ),
+                                          decoration: InputDecoration(
+                                              border: InputBorder.none,
+                                              hintText: "Usuario",
+                                              fillColor: Colors.white,
+                                              filled: true
+                                          ),
+                                        ),
+                                      ),
+                                    )
+                                  ],
                                 ),
                               ),
                             )
                           ],
-                        ),
-                      ),
-                    )
-                  ],
-                )
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 8, top:16),
-              child: Form(
-                  child: Column(
-                    children: <Widget>[
-                      Container(
-                        width: 300,
-                        child: Material(
-                          elevation: 5.0,
-                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                          color: Colors.blue,
-                          child: Row(
-                            children: <Widget>[
-                              Padding(
-                                padding: EdgeInsets.all(12),
-                                child: Icon(Icons.vpn_key,
-                                  color: Colors.white,
+                        )
+                    ),
+                    Padding(
+                        padding: EdgeInsets.only(left: 8, top:16),
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              width: 300,
+                              child: Material(
+                                elevation: 5.0,
+                                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                                color: Colors.blue,
+                                child: Row(
+                                  children: <Widget>[
+                                    Padding(
+                                      padding: EdgeInsets.all(12),
+                                      child: Icon(Icons.vpn_key,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                    Container(
+                                      width: 252,
+                                      height: 60,
+                                      decoration: BoxDecoration(
+                                          color: Colors.white,
+                                          borderRadius: BorderRadius.only(
+                                              bottomRight: Radius.circular(10),
+                                              topRight: Radius.circular(10)
+                                          )
+                                      ),
+                                      child: Padding(
+                                        padding: EdgeInsets.all(8),
+                                        child: TextFormField(
+                                          validator: (value)=> value.isEmpty ? "Contraseña es obligatoria!": null,
+                                          onSaved: (value) => _password = value,
+                                          style:TextStyle(
+                                              color: Colors.black,
+                                              fontSize: 18
+                                          ),
+                                          obscureText: true,
+                                          decoration: InputDecoration(
+                                              border: InputBorder.none,
+                                              hintText: "Contraseña",
+                                              fillColor: Colors.white,
+                                              filled: true
+                                          ),
+                                        ),
+                                      ),
+                                    )
+                                  ],
                                 ),
                               ),
-                              Container(
-                                width: 252,
-                                height: 60,
-                                decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    borderRadius: BorderRadius.only(
-                                        bottomRight: Radius.circular(10),
-                                        topRight: Radius.circular(10)
-                                    )
-                                ),
-                                child: Padding(
-                                  padding: EdgeInsets.all(8),
-                                  child: TextFormField(
-                                    style:TextStyle(
-                                        color: Colors.black,
-                                        fontSize: 18
-                                    ),
-                                    obscureText: true,
-                                    decoration: InputDecoration(
-                                        border: InputBorder.none,
-                                        hintText: "Contraseña",
-                                        fillColor: Colors.white,
-                                        filled: true
-                                    ),
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      )
-                    ],
-                  )
-              ),
+                            )
+                          ],
+                        )
+                    ),
+                  ],
+                ),
+              )
             ),
             Padding(
               padding: EdgeInsets.only(top:20,left: 8, right: 8),
@@ -169,10 +183,29 @@ class LoginPage extends StatelessWidget{
                     style: TextStyle(fontSize: 18, color: Colors.white),
                   ),
                   onPressed: (){
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => PlacePage())
-                    );
+                    final form = _formKey.currentState;
+                    if(form.validate()){
+                      form.save();
+                      if(_user=="alejandro" && _password=="soto123"){
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => PlacePage())
+                        );
+                      }else{
+
+                       /*
+                         Scaffold.of(context).showSnackBar(SnackBar(
+                          backgroundColor: Colors.red,
+                          content: Text("Usuario y/o contraseña incorrecta!"),
+                        ));*/
+
+                        _scaffoldKey.currentState.showSnackBar(SnackBar(
+                          backgroundColor: Colors.red,
+                          duration: Duration(seconds: 2),
+                          content: Text("Usuario y/o contraseña incorrecta!"),
+                        ));
+                      }
+                    }
                   },
                 ),
               )
