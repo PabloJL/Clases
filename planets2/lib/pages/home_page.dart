b327ec6ead2b32963bd5ee33817fcf50fbbb69b4
Ing. Alejandro Soto <soto@itsaltillo.edu.mx>
2020-03-04 20:01:53 -0600

    Card Content

--- planets2/lib/pages/home_page.dart
+++ planets2/lib/pages/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:planets2/gradient_app_bar.dart';
-import 'package:planets2/planet_list.dart';
+import 'package:planets2/components/gradient_app_bar.dart';
+import 'package:planets2/components/planet_list.dart';
 
 class HomePage extends StatelessWidget {
   @override
